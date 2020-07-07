340d59880e88d76c6a7e64b89bb339dd7a2dee20
Lysdel Tellez <ltellez1515@gmail.com>
2020-07-07 20:26:10 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import csv
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+
+dataframe = pd.read_csv("titanic.csv")
+
+# This code replaces the 0 and 1 values in graph with Survived and Did Not Survive
+dataframe = dataframe.replace({"Survived": {0: "Did Not Survive", 1: "Survived"}})
+
+sns.scatterplot(x="Age", y="Fare", hue="Survived", data=dataframe)
+plt.xlabel("Passenger Age")
+plt.ylabel("Passenger Fare")
+plt.savefig("titanic.png")
